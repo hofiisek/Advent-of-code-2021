2a005a97f7f03892240fd7481c1df25963f7f7ba
DHf | Dominik Hoftych <dominik.hoftych@etnetera.cz>
2021-12-05 10:06:00 +0100

    day 4 improvements

--- src/main/kotlin/advent/day4/GiantSquid.kt
+++ src/main/kotlin/advent/day4/GiantSquid.kt
@@ -7,7 +7,7 @@ import java.io.File
  * @author Dominik Hoftych
  */
 
-fun part1(input: File) {
+fun part1(input: File): Int {
     val inputLines = input.readLines()
     val numbers = inputLines.first().split(",").map { it.toInt() }
     val boards = constructBoards(inputLines.drop(1))
@@ -16,42 +16,43 @@ fun part1(input: File) {
         boards.forEach { board ->
             board.markValueIfPresent(number)
             if (board.bingo()) {
-                println(number * board.sumAllUnmarked())
-                return
+                return number * board.sumAllUnmarked()
             }
         }
     }
+
+    throw IllegalArgumentException("No winning board")
 }
 
-fun part2(input: File) {
+fun part2(input: File): Int {
     val inputLines = input.readLines()
     val numbers = inputLines.first().split(",").map { it.toInt() }
     val boards = constructBoards(inputLines.drop(1))
 
-    val winningBoardScores: LinkedHashMap<Int, Int> = linkedMapOf()
+    val winningBoardScores = linkedMapOf<Board, Int>()
     numbers.forEach { number ->
-        boards.forEachIndexed { idx, board ->
+        boards.forEach { board ->
             when {
-                winningBoardScores.contains(idx) -> return@forEachIndexed
+                winningBoardScores.contains(board) -> return@forEach
                 else -> {
                     board.markValueIfPresent(number)
                     if (board.bingo()) {
-                        winningBoardScores[idx] = number * board.sumAllUnmarked()
+                        winningBoardScores[board] = number * board.sumAllUnmarked()
                     }
                 }
             }
         }
     }
 
-    println(winningBoardScores.values.lastOrNull() ?: throw IllegalArgumentException("No winning board"))
+    return winningBoardScores.values.lastOrNull() ?: throw IllegalArgumentException("No winning board")
 }
 
 fun constructBoards(inputLines: List<String>): List<Board> = inputLines
     .filter { it.isNotBlank() }
     .chunked(5)
-    .map { chunk ->
+    .map { board ->
         // chunk of 5 rows as strings
-        chunk.map { rowStr ->
+        board.map { rowStr ->
             rowStr.split("\\s+".toRegex())
                 .filter { it.isNotBlank() }
                 .map { Cell(it.toInt()) }
@@ -61,7 +62,7 @@ fun constructBoards(inputLines: List<String>): List<Board> = inputLines
 fun main() {
 //    with(loadInput(day = 4, filename = "input_example.txt")) {
     with(loadInput(day = 4)) {
-        part1(this)
-        part2(this)
+        println(part1(this))
+        println(part2(this))
     }
 }
