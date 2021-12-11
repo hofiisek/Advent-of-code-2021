ed39d6e031d28be12b7016c1344765bea99584e5
DHf | Dominik Hoftych <dominik.hoftych@etnetera.cz>
2021-12-11 18:30:39 +0100

    extracted Matrix and Position classes

--- src/main/kotlin/advent/day9/SmokeBasin.kt
+++ src/main/kotlin/advent/day9/SmokeBasin.kt
@@ -1,41 +1,12 @@
 package advent.day9
 
+import advent.Matrix
+import advent.Position
+import advent.adjacents
 import advent.loadInput
 import java.io.File
-import kotlin.collections.ArrayDeque
 
-/**
- * @author Dominik Hoftych
- */
-
-data class Position(val row: Int, val col: Int)
 data class Point(val height: Int, val position: Position)
-
-class Matrix<T>(private val elements: List<List<T>>): List<List<T>> by elements {
-
-    private val rows: Int = elements.size
-    private val cols: Int = elements.firstOrNull()?.size ?: 0
-
-    init {
-        if (elements.isNotEmpty() && elements.any { row -> row.size != elements.first().size })
-            throw IllegalArgumentException("Some rows have different sizes")
-    }
-
-    operator fun get(position: Position) =
-        if (position.row < 0 || position.col < 0 || position.row >= rows || position.col >= cols)
-            null
-        else
-            this[position.row][position.col]
-
-    fun getOrThrow(position: Position) = get(position)
-        ?: throw IllegalArgumentException("Position $position out of bounds")
-}
-
-fun <T> Matrix<T>.adjacents(position: Position): Set<T> = listOf(-1, 1)
-    .flatMap { listOf(Position(position.row + it, position.col), Position(position.row, position.col + it)) }
-    .mapNotNull(this::get)
-    .toSet()
-
 
 /**
  * Just calculate the risk level for all points that are lower than all of their adjacent point
