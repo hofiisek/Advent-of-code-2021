71d280d2a4b8f654bd8917f85c91083baac92726
DHf | Dominik Hoftych <dominik.hoftych@etnetera.cz>
2021-12-13 09:09:18 +0100

    day 12 part 2 recursively with mutable state

--- src/main/kotlin/advent/day12/PassagePathing.kt
+++ src/main/kotlin/advent/day12/PassagePathing.kt
@@ -2,12 +2,15 @@ package advent.day12
 
 import advent.loadInput
 import java.io.File
+import kotlin.system.measureTimeMillis
+import kotlin.time.measureTimedValue
 
 /**
  * @author Dominik Hoftych
  */
 
 typealias Path = List<String>
+operator fun Set<Path>.plus(other: Path): Set<Path> = this + setOf(other)
 
 typealias Graph = Map<String, Set<String>>
 fun Graph.getEdges(node: Node): Set<Node> = get(node.id)
@@ -15,7 +18,7 @@ fun Graph.getEdges(node: Node): Set<Node> = get(node.id)
         when {
             edgeTo == "start" -> StartCave(edgeTo, node.path + edgeTo)
             edgeTo == "end" -> EndCave(edgeTo, node.path + edgeTo)
-            edgeTo.all { it.isLowerCase() }-> SmallCave(edgeTo, node.path + edgeTo)
+            edgeTo.all(Char::isLowerCase) -> SmallCave(edgeTo, node.path + edgeTo)
             else -> BigCave(edgeTo, node.path + edgeTo)
         }
     }
@@ -25,59 +28,101 @@ fun Graph.getEdges(node: Node): Set<Node> = get(node.id)
 sealed class Node {
     abstract val id: String
     abstract val path: Path
+    val pathNodesCount by lazy {
+        path.filter(String::isSmallCave).groupingBy { it }.eachCount()
+    }
 }
 open class SmallCave(override val id: String, override val path: Path): Node()
 data class BigCave(override val id: String, override val path: Path): Node()
 data class StartCave(override val id: String, override val path: Path = listOf(id)): SmallCave(id, path)
 data class EndCave(override val id: String, override val path: Path): SmallCave(id, path)
+fun String.isSmallCave() = all(Char::isLowerCase)
 
 operator fun <K> Map<K, Set<K>>.plus(other: Map<K, Set<K>>) =
     this + other.map { (k, v) -> k to (getOrDefault(k, emptySet()) + v) }
+
+fun <E> queueOf(element: E): ArrayDeque<E> = ArrayDeque(listOf(element))
+fun <E> queueOf(elements: List<E>): ArrayDeque<E> = ArrayDeque(elements)
 
 data class IntermediateState(
     val graph: Graph,
     val queue: List<Node> = listOf(StartCave("start")),
     val completePaths: Set<Path> = emptySet()
 )
-fun Graph.toIntermediateState() = IntermediateState(graph = this)
+fun Graph.toMutableIntermediateState() = MutableIntermediateState(graph = this)
 
 @OptIn(ExperimentalStdlibApi::class)
-fun part1(input: File) = input.readLines()
-    .map { it.split("-") }
-    .fold(emptyMap<String, Set<String>>()) { graph, (edgeFrom, edgeTo) ->
-        graph + mapOf(edgeFrom to setOf(edgeTo)) + mapOf(edgeTo to setOf(edgeFrom))
-    }
-    .onEach(::println)
+fun part1(input: File) = loadGraph(input)
     .let(Graph::toIntermediateState)
-    .let(::findAllPaths)
+    .let { initialState ->
+        DeepRecursiveFunction<IntermediateState, Set<Path>> { state ->
+            val (graph, queue, completePaths) = state
+            when {
+                queue.isEmpty() -> completePaths
+                else -> {
+                    val curr = queue.first()
+                    if (curr is EndCave) {
+                        callRecursive(state.copy(queue = queue.drop(1), completePaths = completePaths + curr.path))
+                    } else {
+                        val edges = graph.getEdges(curr)
+                            .filterNot { edgeTo -> curr.path.contains(edgeTo.id) && edgeTo is SmallCave }
+
+                        callRecursive(state.copy(queue = queue.drop(1) + edges))
+                    }
+                }
+            }
+        }.invoke(initialState)
+    }
     .onEach(::println)
     .count()
 
-@OptIn(ExperimentalStdlibApi::class)
-typealias DeepRecursiveBfs = DeepRecursiveFunction<IntermediateState, Set<Path>>
+data class MutableIntermediateState(
+    val graph: Graph,
+    val queue: ArrayDeque<Node> = queueOf(StartCave("start")),
+    val completePaths: MutableSet<Path> = mutableSetOf()
+)
+fun Graph.toIntermediateState() = IntermediateState(graph = this)
 
 @OptIn(ExperimentalStdlibApi::class)
-fun findAllPaths(initialState: IntermediateState) =  DeepRecursiveBfs { state ->
-        val (graph, queue, completePaths) = state
-        when {
-            queue.isEmpty() -> completePaths
-            else -> {
-                val curr = queue.first()
-                if (curr is EndCave) {
-                    callRecursive(
-                        state.copy(queue = queue.drop(1), completePaths = completePaths + setOf(curr.path))
-                    )
-                } else {
-                    val edges = graph.getEdges(curr)
-                        .filterNot { edgeTo -> curr.path.contains(edgeTo.id) && edgeTo is SmallCave }
+fun part2(input: File) = loadGraph(input)
+    .let(Graph::toMutableIntermediateState)
+    .let { initialState ->
+        DeepRecursiveFunction<MutableIntermediateState, MutableSet<Path>> { state ->
+            val (graph, queue, completePaths) = state
+            when {
+                queue.isEmpty() -> completePaths
+                else -> {
+                    val curr = queue.removeFirst()
+                    if (curr is EndCave) {
+                        completePaths.add(curr.path)
+                        callRecursive(state)
+                    } else {
+                        val edges = graph.getEdges(curr)
+                            .filterNot { edgeTo ->
+                                when (edgeTo) {
+                                    is StartCave, is EndCave -> curr.path.contains(edgeTo.id)
+                                    is SmallCave ->
+                                        curr.path.contains(edgeTo.id)
+                                        && curr.pathNodesCount.any { (_, count) -> count >= 2 }
+                                    is BigCave -> false
+                                }
+                            }
 
-                    callRecursive(state.copy(queue = queue.drop(1) + edges))
+                        queue.addAll(edges)
+                        callRecursive(state)
+                    }
                 }
             }
-        }
-    }.invoke(initialState)
+        }.invoke(initialState)
+    }
+//    .onEach(::println)
+    .count()
 
-fun part2(input: File) = Unit
+fun loadGraph(input: File): Graph = input.readLines()
+    .map { it.split("-") }
+    .fold(emptyMap()) { graph, (edgeFrom, edgeTo) ->
+        graph + mapOf(edgeFrom to setOf(edgeTo)) + mapOf(edgeTo to setOf(edgeFrom))
+    }
 
 fun main() {
 //    with(loadInput(day = 12, filename = "input_example.txt")) {
@@ -85,6 +130,9 @@ fun main() {
 //    with(loadInput(day = 12, filename = "input_example3.txt")) {
     with(loadInput(day = 12)) {
         println(part1(this))
-        println(part2(this))
+        val time = measureTimeMillis {
+            println(part2(this))
+        }
+        println(time)
     }
 }
